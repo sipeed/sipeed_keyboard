58034c6448c6113b8a7315fb18143010573a4c5f
HuakeZhBo <zb2691435029@163.com>
2021-10-13 12:57:15 +0800

    shell update

--- firmware/keyboard/sipeed_keyboard_68/include/smk_cdc.h
+++ firmware/keyboard/sipeed_keyboard_68/include/smk_cdc.h
@@ -4,7 +4,9 @@
 #define CDC_IN_EP  0x84
 #define CDC_OUT_EP 0x03
 #define CDC_INT_EP 0x85
+#define SHELL_NAME            "sipeed"
 
+void acm_printf(char *fmt, ...);
 void smk_cdc_init();
 
 #endif
--- firmware/keyboard/sipeed_keyboard_68/include/smk_command.h
+++ firmware/keyboard/sipeed_keyboard_68/include/smk_command.h
@@ -1,4 +1,4 @@
-#include "smk_shell.h"
+#include "shell.h"
 
 void hellowd()
 {
